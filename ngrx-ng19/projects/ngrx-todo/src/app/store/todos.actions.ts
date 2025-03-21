7d25cf06d6a257382e94965e79a1c0373b1c008a
sbtalk71 <sbtalk@yahoo.com>
2025-03-21 09:43:32 +0530

    ssr demo commit

--- ngrx-ng19/projects/ngrx-todo/src/app/store/todos.actions.ts
+++ ngrx-ng19/projects/ngrx-todo/src/app/store/todos.actions.ts
@@ -3,4 +3,4 @@ import { createAction,props } from "@ngrx/store";
 
 export const addTodo=createAction("[ToDo] add",props<{text:string}>())
 export const toggleToDo=createAction("[ToDo] toggle",props<{id:number}>())
-export const removeToDo=createAction("[ToDo] add",props<{id:number}>())
+export const removeToDo=createAction("[ToDo] remove",props<{id:number}>())
